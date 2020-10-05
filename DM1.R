9c55a9a3c71d77102e15d5f9cdee0bd22d99c213
MonkeyDflo <florent.jakubowki@gmail.com>
2020-10-05 05:33:38 -0700

    Ajout DM1 et correction TP4

--- DM1.R
+++ DM1.R
@@ -9,6 +9,7 @@ simuRejection <- function(k, n, seed, isPlotted = FALSE){
   #k dégrés du polynôme
   #n nombres de point à tester
   #seed graine pour la génération aléatoire
+  #isPlotted Booléen pour afficher ou non le plot à chaque étape
   
   #1ère étape : générer n couple (u,v) aléatoire avec u et v compris [0;1]
   set.seed(seed)
@@ -18,23 +19,12 @@ simuRejection <- function(k, n, seed, isPlotted = FALSE){
   df <- data.frame(u, v, isinDk)
   
   #condition
-  
-  #try to use apply on df
-  # test <- function(u,v){
-  #   if ( u^(1/k) + v^(1/k) <= 1 ){
-  #     return(u,v)
-  #   }
-  # }
-  #apply(df, 1, function(df) test(df['u'],df['v'],df['isinDk']) )
-  #df$isinDk <- apply(df[,1:2], 1, test(df$[u],df$[v]))
-  
   for(i in 1:n){
     if ( df$u[i]^(1/k) + df$v[i]^(1/k) <= 1 ){
       df$isinDk[i] <- TRUE
     }
   }
   
-  # print(df[(df$isinDk == TRUE),])
   if(isPlotted) {plot(df[(df$isinDk == TRUE),])}
   
 }
@@ -52,14 +42,30 @@ estimateTime <- function(k, n, seed, isPlotted = FALSE){
 k <- c(1,2,3,4,5,10,20, 30, 40)
 
 estimateTime(k, 10000, 7, TRUE)
-a
 
-#install.packages("microbenchmark")
-#install.packages("ggplot2")
-# library(microbenchmark)
-# library(ggplot2)
-# m <- microbenchmark(estimateTime(k, 10000, 7),
-#                     times = 1)
-# ggplot2::autoplot(m)
+#II Echantillonage de Gibbs####
 
-#
+gibbs<-function (n) 
+{
+  mat <- matrix(ncol = 2, nrow = n)
+  x <- 0
+  y <- 0
+  mat[1, ] <- c(x, y)
+  for (i in 2:n) {
+    x <- runif(n) 
+    y <- runif(n)
+    mat[i, ] <- c(x, y)
+  }
+  mat
+}
+
+bvn<-gibbs(10000)
+par(mfrow=c(3,2))
+plot(bvn,col=1:10000)
+plot(bvn,type="l")
+plot(ts(bvn[,1]))
+plot(ts(bvn[,2]))
+hist(bvn[,1],40)
+hist(bvn[,2],40)
+par(mfrow=c(1,1))
+
--- TP4.R
+++ TP4.R
@@ -68,3 +68,182 @@ lnks <- matrix(rnorm(500^2), nrow = 500) #rnorm créé un vecteur de 500^2
 mutoutpar(cl, lnks)
 
 #deparse(substitute(links))
+
+
+## Correction ####
+
+## File: pc4ds-lab4.r
+## Description: Lab 4 of Parallel Computing for Data Science
+## Date:  Nov 2016 by jc
+
+rm(list = ls())
+
+# Ex1. Que fait ce code? ####
+dormir <- function(i) {
+  Sys.sleep(i)
+  return(paste("le fils", Sys.getpid(), "a dormi", i, "secondes"))
+}
+
+temps <- list(5, 30, 5, 10)
+
+library(parallel)
+# Avec multicore (si disponible)
+# mclapply(temps, dormir, mc.cores = 2)
+# Avec SNOW
+cl <- makeCluster(2)
+system.time(res <- parLapply(cl, temps, dormir))
+stopCluster(cl)
+
+temps2 <- list(c(5,5,10), 30) 
+cl <- makeCluster(2)
+system.time(res2 <- parLapply(cl, temps2, dormir))
+stop(cl)
+
+
+## Ex. 2 ####
+doichunk <- function(ichunk) {
+  tot <- 0
+  nr  <- nrow(lnks) # lnks global at worker
+  
+  for(i in ichunk) {
+    tmp <- lnks[(i + 1):nr , ] %*% lnks[i , ]
+    tot <- tot + sum(tmp)
+  }
+  tot
+}
+
+mutoutpar <- function(cls, lnks) {
+  require(parallel)
+  
+  nr <- nrow(lnks) # lnks global at worker
+  clusterExport(cls, "lnks")
+  
+  ichunks <- 1:(nr - 1) # each "chunk" has only 1 value of i , for now
+  tots <- clusterApply(cls, ichunks, doichunk)
+  
+  Reduce(sum, tots ) / nr
+}
+
+mutoutparB <- function(cls, lnks) {
+  require(parallel)
+  
+  nr <- nrow(lnks) # lnks global at worker
+  clusterExport(cls, "lnks")
+  
+  ichunks <- (nr - 1):1 # each "chunk" has only 1 value of i , for now
+  tots <- clusterApply(cls, ichunks, doichunk)
+  
+  Reduce(sum, tots ) / nr
+}
+
+mutoutparC <- function(cls, lnks) {
+  require(parallel)
+  
+  nr <- nrow(lnks) # lnks global at worker
+  clusterExport(cls, "lnks")
+  
+  ichunks <- clusterSplit(cls, 1:(nr - 1)) # each "chunk" has only 1 value of i , for now
+  tots <- clusterApply(cls, ichunks, doichunk)
+  
+  Reduce(sum, tots ) / nr
+}
+
+mutoutparD <- function(cls, lnks) {
+  require(parallel)
+  
+  nr <- nrow(lnks) # lnks global at worker
+  clusterExport(cls, "lnks")
+  
+  smp <- sample(nr - 1)
+  ichunks <- clusterSplit(cls, smp) 
+  tots <- clusterApply(cls, ichunks, doichunk)
+  
+  Reduce(sum, tots ) / nr
+}
+
+mutoutparE <- function(cls, lnks) {
+  require(parallel)
+  
+  nr <- nrow(lnks) # lnks global at worker
+  clusterExport(cls, "lnks")
+  
+  ichunks <- lapply(1:length(cls), function(i) seq(i, nr - 1, by = i))
+  tots <- clusterApply(cls, ichunks, doichunk)
+  
+  Reduce(sum, tots ) / nr
+}
+
+## Benchmark and timings with a simulated dataset ####
+
+# Simulate a link matrix of size n (e.g. n = 500)
+n   <- 500
+lnks <- matrix(sample(0:1 , n^2, replace = TRUE), nrow = n)
+
+#calcul séquentiel
+system.time(lapply(1:(nrow(lnks) - 1), function(i) doichunk(i)))
+
+## Paralle computation with snow
+library(parallel)
+cls <- makeCluster(4) 
+system.time(mutoutpar(cls, lnks))
+system.time(mutoutparB(cls, lnks))
+system.time(mutoutparC(cls, lnks))
+system.time(mutoutparD(cls, lnks))
+system.time(mutoutparE(cls, lnks))
+
+
+library(microbenchmark)
+library(ggfortify)
+compare <- microbenchmark(mutoutpar(cls, lnks), 
+                          mutoutparB(cls, lnks), 
+                          mutoutparC(cls, lnks),
+                          mutoutparD(cls, lnks),
+                          mutoutparE(cls, lnks),
+                          times = 10)
+autoplot(compare)
+stopCluster(cls); rm(cls)
+
+# You may use microbenchmark to have more significant timmings
+
+## Exercice 3 ####
+leave.one.out <- function(i, dataset) {
+  fit   <- lm(Petal.Width ~ Petal.Length, data = dataset[-i, ]) 
+  pred  <- predict(fit, data.frame(Petal.Length = dataset[i, "Petal.Length"]))
+  error <- (pred - dataset[i, "Petal.Length"]) ^ 2
+  return(error)
+}
+
+compute.PRESS <- function(dataset) {
+  Reduce("+", lapply(1:nrow(dataset), function (i) leave.one.out(i, dataset)))
+}
+
+simuData <- function(sizes){
+  model <- lm(Petal.Width ~ Petal.Length, data = iris)
+  lapply(sizes, function (n) {
+    a <- min(iris[, "Petal.Length"])
+    b <- max(iris[, "Petal.Length"])
+    iris2 <- data.frame(Petal.Width = rep(NA, n),
+                        Petal.Length = runif(n, a, b))
+    iris2[, "Petal.Width"] <- 
+      predict(model, data.frame(Petal.Length = iris2[, "Petal.Length"]))
+    return(iris2)
+  })
+}
+
+
+library(parallel)
+
+
+# Situation 1 
+multiple.iris <- simuData(rep(200, 8))
+
+cl <- makeCluster(4)
+...
+stopCluster(cl)
+
+# foreach parallel
+library(foreach)    # provides foreach
+library(doParallel) # provides %dopar%
+registerDoParallel(cores = 4) 
+...
+stopCluster(cl)
